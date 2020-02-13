1896da4594b1a8012372ddfbe3adf47a048690a3
SimonLi <chenmoonmo@gmail.com>
2020-02-13 21:41:27 +0800

    import

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log('ok');
-import m1 from './m1.js';
-console.log(m1);
-console.log(m1.a);
-import {s1,s2 as d,say} from './m1.js'
-console.log(d)
-say();
+// m1.js
+// import m1 from './m1.js';
+// console.log(m1);
+// console.log(m1.a);
+// import {s1,s2 as d,say} from './m1.js'
+// console.log(d)
+// say();
+//m2.js
+import './m2.js'
